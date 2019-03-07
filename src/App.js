9ca5233a320d3dab84ba4b6837475b291b15c22f
xiuw <sylvi.xw@gmail.com>
2019-03-07 13:16:03 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {quotes} from './quote';
+import { SocialIcon } from 'react-social-icons';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+
+	constructor(){
+		super();
+		this.state= {
+			random:Math.floor(Math.random() * quotes.length)}
+
+	} 
+
+	getRandom =()=>{
+		
+
+	}
+ 
+	 handleClick=()=>{
+	 	let random = Math.floor(Math.random() * quotes.length);
+
+		 	do{
+		 	 random = Math.floor(Math.random() * quotes.length)
+		 	}while(random === this.state.random)
+	 
+	 	
+ 		this.setState({random});
+	   
+	 }
+
+	render() {
+	  	const{random} = this.state;
+
+	    return (
+	      <div className='App' id='quote-box'>
+
+		   	<div className='quoteCSS'>
+			   	<SocialIcon id='tweet-quote'bgColor="#EB783D" fgColor="fff" style={{ height: 30, width: 30 }} network="twitter" url='http://twitter.com/intent/tweet'/>
+					<p id='text'>" {quotes[random].quote}"</p>
+					<p id='author'>- {quotes[random].author}</p>
+					<p><button id='new-quote'onClick={this.handleClick}>New Quote</button></p>
+			 	</div>
+
+		  </div>
+ 	
+	    );
+	  }
+
 }
 
 export default App;
---
+++ src/quote.js
@@ -0,0 +1,47 @@
+export const quotes = [
+
+	
+		{
+			id:1,
+			quote: 'The more you feed your mind with positive thoughts, the more you can attract great things into your life.',
+			author:'Roy T. Bennett'
+
+		},
+
+		{
+			id:2,
+			quote: 'If you can tell stories, create characters, devise incidents, and have sincerity and passion, it doesn’t matter a damn how you write.',
+			author:'Somerset Maugham'
+
+		},
+
+
+		{
+			id:3,
+			quote: 'Be happy for this moment. This moment is your life.',
+			author:'Omar Khayyam'
+		},
+
+		{
+			id:4,
+			quote: 'If opportunity doesn’t knock, build a door.',
+			author:'Milton Berle'
+			
+
+		},
+
+		{
+			id:5,
+			quote: 'Always say ‘yes’ to the present moment… Surrender to what is. Say ‘yes’ to life – and see how life starts suddenly to start working for you rather than against you.',
+			author:'Eckhart Tolle'
+
+		},
+
+
+
+
+
+
+
+
+]
