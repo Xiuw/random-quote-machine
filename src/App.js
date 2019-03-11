0ad6d28f4cc0c235a0d6846f571c1e304bb641dc
xiuw <sylvi.xw@gmail.com>
2019-03-10 23:04:40 -0400

    Add new quote and successfully submit to the database

--- src/App.js
+++ src/App.js
@@ -10,14 +10,31 @@ class App extends Component {
 	constructor(){
 		super();
 		this.state= {
+		
+			quote:'',
+			author:'',
 			random:Math.floor(Math.random() * quotes.length)}
 
 	} 
 
-	getRandom =()=>{
-		
+	onHandleChange=(e)=>{
+		const {name,value} = e.target
+		console.log(value);
+		this.setState({[name]:value});
+	}
+	onHandleSubmit = (e) => {
+		fetch('http://localhost:3000/submitQuote',{
+			method:'post',
+			headers:{'Content-Type':'Application/json'},
+			body:JSON.stringify({
+				quote:this.state.quote,
+				author:this.state.author
+			})
+		}).then(response=>response.json())
+		.then(data=>console.log(data));
 
 	}
+
  
 	 handleClick=()=>{
 	 	let random = Math.floor(Math.random() * quotes.length);
@@ -32,11 +49,16 @@ class App extends Component {
 	 }
 
 	render() {
+		
 	  	const{random} = this.state;
 
 	    return (
-	      <div className='App' id='quote-box'>
-
+	    <div className='App'> 
+	     	<textarea className ='textArea' name='quote' value ={this.state.quote} type='text' rows='6' cols='60'onChange={this.onHandleChange}/>
+	     	<input className='textArea' type='text' name ='author' value={this.state.author} onChange={this.onHandleChange} />
+	     	<button className='submit'onClick={this.onHandleSubmit} >Submit</button>
+	      
+	      <div  id='quote-box'>
 		   	<div className='quoteCSS'>
 			   	<SocialIcon id='tweet-quote'bgColor="#EB783D" fgColor="fff" style={{ height: 30, width: 30 }} network="twitter" url='http://twitter.com/intent/tweet'/>
 					<p id='text'>" {quotes[random].quote}"</p>
@@ -45,6 +67,7 @@ class App extends Component {
 			 	</div>
 
 		  </div>
+		 </div> 
  	
 	    );
 	  }
