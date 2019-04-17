fd5ca31a539ca5ac6a3919fd5c3aff2b1c4ad502
xiuw <sylvi.xw@gmail.com>
2019-04-17 02:37:53 -0400

    Updates

---
+++ precache-manifest.dd94b6d414f8025b6b23f36aae26fbf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  },
+  {
+    "revision": "837364e607bef721a75a",
+    "url": "/random-quote-machine/static/js/main.989d2f25.chunk.js"
+  },
+  {
+    "revision": "701064b27df928a0a44a",
+    "url": "/random-quote-machine/static/js/2.44b91398.chunk.js"
+  },
+  {
+    "revision": "837364e607bef721a75a",
+    "url": "/random-quote-machine/static/css/main.d9e3c1ac.chunk.css"
+  },
+  {
+    "revision": "51402ec53316cc075adf2d57dbb34818",
+    "url": "/random-quote-machine/index.html"
+  }
+];
